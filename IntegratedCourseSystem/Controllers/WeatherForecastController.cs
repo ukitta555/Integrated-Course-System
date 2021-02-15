9bf5061efb0f8365facb16e4a9b2b3e6e84f322a
Yaroslav Prykhodko <you@example.com>
2021-02-15 19:53:22 +0200

    Fixed version .NET Core 3.1 -> .NET 5

--- IntegratedCourseSystem/Controllers/WeatherForecastController.cs
+++ IntegratedCourseSystem/Controllers/WeatherForecastController.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace IntegratedCourseSystem.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -34,27 +34,6 @@ namespace IntegratedCourseSystem.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
-        }
-    }
-
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController
-    {
-        private List<int> numbers = new List<int>();
-
-        [HttpGet]
-        public IEnumerable<int> Get()
-        {
-            numbers.Add(4);
-            return numbers;
-        }
-
-        [HttpPost("{number}")] 
-        public IEnumerable<int> Post(int number)
-        {
-            numbers.Add(number);
-            return Enumerable.Range(0,number).ToArray();
         }
     }
 }
