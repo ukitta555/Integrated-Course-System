655355dc2814ef80506b871d7190fbcf1aafefd0
Vladyslav Nekriach <ukitta555@gmail.com>
2021-02-16 16:47:37 -0200

    add sample to-do-list API using MSDN docs

--- IntegratedCourseSystem/Startup.cs
+++ IntegratedCourseSystem/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using IntegratedCourseSystem.Models;
 
 namespace IntegratedCourseSystem
 {
@@ -21,13 +23,15 @@ namespace IntegratedCourseSystem
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllers();
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/build";
             });
+            services.AddDbContext<TodoContext>(opt =>
+                                               opt.UseInMemoryDatabase("TodoList"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
