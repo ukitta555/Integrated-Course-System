a36cc78f754233b470731ac35ca448bb2b2a77c7
Vladyslav Nekriach <ukitta555@gmail.com>
2021-02-18 14:11:16 -0200

    add material ui to header

--- IntegratedCourseSystem/ClientApp/src/components/HeaderFolder/Header.tsx
+++ IntegratedCourseSystem/ClientApp/src/components/HeaderFolder/Header.tsx
@@ -7,26 +7,60 @@ import {
   from 'react-router-dom'
 import ImageFitToParent from '../ImageFitToParent/ImageFitToParent'
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/Button';
+
+import Menu from "@material-ui/icons/Menu";
+
+
 const Header = () => {
 
-
-  const divStyle = {
+  const logoDivStyle = {
       width: 50,
       height: 50
   }
 
+  const headerWrapperStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'spece-between'
+  }
 
   return (
     <div>
-      <div style = {divStyle}>
-          <Link to = '/'>
-              <ImageFitToParent src = './img/logo.png' alt = 'logo'/>
-          </Link>
-      </div>
-      T-Collab
+      <AppBar position="static">
+        <Toolbar>
 
-      <Link to = '/register'> register </Link>
-      <Link to = '/login'> login </Link>
+          <div style = {logoDivStyle}>
+            <Link to = '/'>
+              <ImageFitToParent src = './img/logo.png' alt = 'logo'/>
+            </Link>
+          </div>
+
+
+
+          <Typography variant="h6">
+            T-Collab
+          </Typography>
+
+          <Button color = "inherit">
+            <Link to = '/register'>
+              register
+            </Link>
+          </Button>
+
+          <Button color = "inherit">
+            <Link to = '/login'>
+              login
+            </Link>
+          </Button>
+
+        </Toolbar>
+      </AppBar>
+
 
       <Switch>
           <Route path = '/register'>
@@ -39,6 +73,7 @@ const Header = () => {
               bruh
           </Route>
       </Switch>
+
     </div>
   )
 }
