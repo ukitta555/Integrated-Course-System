d89f4e9800edbe061c69aa29aaba70f90c074734
Vladyslav Nekriach <ukitta555@gmail.com>
2021-02-19 14:37:28 -0200

    add registration and login forms

---
+++ IntegratedCourseSystem/ClientApp/src/hooks/useField.tsx
@@ -0,0 +1,17 @@
+import {useState} from 'react'
+
+const useField = (type: string) => {
+  const [value, setValue] = useState('');
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value)
+  }
+
+  return ({
+    type,
+    value,
+    onChange
+  })
+}
+
+export default useField
