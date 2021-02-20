001c4b95d84578193c6f924480426086907297c7
Yaroslav Prykhodko <you@example.com>
2021-02-20 18:48:51 +0200

    Tested database context. Work adding, removing entities.

--- IntegratedCourseSystem/Extension/IServiceCollectionExtensions.cs
+++ IntegratedCourseSystem/Extension/IServiceCollectionExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using IntegratedCourseSystem.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,8 +12,12 @@ namespace IntegratedCourseSystem.Extensions
 {
     public static class IServiceCollectionExtensions
     {
-        public static void AddIntegratedCourseSystemContext(this IServiceCollection services)
+        public static void AddIntegratedCourseSystemContext(this IServiceCollection services, IConfiguration configuration)
         {
+            string connection = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<IntegratedCourseSystemContext>(options => options.UseNpgsql(connection));
+
             services.AddDbContext<IntegratedCourseSystemContext>(opt => opt.UseInMemoryDatabase("IntegratedCourseSystem"));
         }
 
