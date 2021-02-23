6b7cfc65238979995c3278c884fa8d0c9bc40f92
Vladyslav Nekriach <ukitta555@gmail.com>
2021-02-23 16:28:09 -0200

    login with fake api works

--- IntegratedCourseSystem/ClientApp/src/reducers/userReducer/userThunks.tsx
+++ IntegratedCourseSystem/ClientApp/src/reducers/userReducer/userThunks.tsx
@@ -3,7 +3,7 @@ import userService from '../../services/userService'
 import {RootState} from '../../store/configureStore'
 import {Action} from 'redux'
 import { ThunkAction } from 'redux-thunk'
-import { createRegisterUserAction} from '../../reducers/userReducer/userActionCreators'
+import { createRegisterUserAction, loginUserAction} from '../../reducers/userReducer/userActionCreators'
 
 export const registerUser = (email: string, password: string, repeatPassword: string) : ThunkAction<void, RootState, unknown, Action<string>> => {
   return async dispatch => {
@@ -14,5 +14,13 @@ export const registerUser = (email: string, password: string, repeatPassword: st
     else {
       console.log ('passwords don\'t match!')
     }
+  }
+}
+
+export const loginUser = (email: string, password: string) : ThunkAction<void, RootState, unknown, Action<string>> => {
+  return async dispatch => {
+    const loginResponse = await userService.login ({email, password});
+    console.log (loginResponse)
+    dispatch(loginUserAction(email, password))
   }
 }
