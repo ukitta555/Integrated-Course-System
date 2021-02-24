f7b78f77d62a3165d6b3d8503c8998915a358b0a
Yaroslav Prykhodko <you@example.com>
2021-02-24 20:58:08 +0200

    Added questionnaire api controller, semi-implemented

--- IntegratedCourseSystem/Models/UserModel/UserDTO.cs
+++ IntegratedCourseSystem/Models/UserModel/UserDTO.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 
 namespace IntegratedCourseSystem.Models.UserModel
 {
+    /// <summary>
+    /// Data Transfer Object for class User
+    /// </summary>
     public class UserDTO
     {
         public string Email { get; set; }
