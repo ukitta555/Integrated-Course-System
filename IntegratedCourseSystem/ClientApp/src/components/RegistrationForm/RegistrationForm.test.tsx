3ad23ce0e39dbaa60ee9bdb833eede2abe15c78f
Andrew <andrew.klyachkin@gmail.com>
2021-02-24 11:09:35 +0200

    Added validation test

---
+++ IntegratedCourseSystem/ClientApp/src/components/RegistrationForm/RegistrationForm.test.tsx
@@ -0,0 +1,12 @@
+import { validateEmail } from "./RegistrationForm";
+
+test('regex test on @knu.ua', () => {
+    expect(validateEmail("andrew@knu.ua")).toBe(true);
+    expect(validateEmail("andrew.kl@knu.ua")).toBe(true);
+    expect(validateEmail("derevianchenko......@knu.ua")).toBe(true);
+    expect(validateEmail("@knu.ua")).toBe(false);
+  });
+
+test('regex test on not @knu.ua', () => {
+    expect(validateEmail("andrew@gmail.com")).toBe(false);
+  });
