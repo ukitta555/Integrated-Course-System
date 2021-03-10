ccf57b17a20be3e33bce08e47515ad2c9b88fc67
vargroozzz <edand2002@gmail.com>
2021-03-10 12:55:21 +0200

    course creating page design

--- IntegratedCourseSystem/ClientApp/src/components/DeletableTextField/DeletableTextField.tsx
+++ IntegratedCourseSystem/ClientApp/src/components/DeletableTextField/DeletableTextField.tsx
@@ -1,13 +1,19 @@
 import React from "react"
-import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import CloseIcon from '@material-ui/icons/Close';
+import InputBase from "@material-ui/core/InputBase";
 import { eventNames } from "process";
 
+const textFieldStyles = {
+    background: "#F5F5F5",
+    borderRadius: "50px",
+    margin: "3% 0",
+}
+
 const DeletableTextField = (props: { id: string, onDelete: () => void, value: string, onChange: (ev: React.ChangeEvent<HTMLInputElement>) => void }) => (
-        <Grid item id={props.id}>
-            <TextField variant="outlined" value = {props.value} onChange = {props.onChange}/>
+        <Grid container item alignItems="stretch" justify="center" id={props.id}>
+            <InputBase style={textFieldStyles} inputProps={{style: {paddingLeft: "1em"}}} value = {props.value} onChange = {props.onChange} />
             <Button startIcon={<CloseIcon/>} onClick={ props.onDelete }/>
         </Grid>
     )
