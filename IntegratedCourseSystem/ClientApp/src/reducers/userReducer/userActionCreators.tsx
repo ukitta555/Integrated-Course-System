92339e1c049ba81fc4d247b5609204f79a92342d
Vladyslav Nekriach <ukitta555@gmail.com>
2021-03-16 16:45:14 -0200

    add name, surname, role and other info to user state

--- IntegratedCourseSystem/ClientApp/src/reducers/userReducer/userActionCreators.tsx
+++ IntegratedCourseSystem/ClientApp/src/reducers/userReducer/userActionCreators.tsx
@@ -1,7 +1,9 @@
 import {
   LOGIN_USER,
+  UPDATE_USER_WITH_QUE_INFO,
   registrationActionTypes,
-  UserState
+  UserState,
+  Role
 } from '../../store/types'
 
 export const loginUserAction = (userInfo: {id: number, email: string}) : registrationActionTypes => {
@@ -9,10 +11,25 @@ export const loginUserAction = (userInfo: {id: number, email: string}) : registr
     ...userInfo,
     name: null,
     surname: null,
-    role: "user"
+    role: "user",
+    teacherId: null,
+    facultyId: null
   }
   return {
     type: LOGIN_USER,
     userInfo: modifiedUserInfo
+  }
+}
+
+export const updateUserWithQueInfo = (userInfo: {
+  name: string,
+  surname: string,
+  role: Role,
+  teacherId: number | null,
+  facultyId: number | null
+}) => {
+  return {
+    type: UPDATE_USER_WITH_QUE_INFO,
+    userInfo
   }
 }
