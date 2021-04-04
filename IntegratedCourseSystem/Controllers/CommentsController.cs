20434d7ee943b44f74f77a5094f4d81a81a17a3c
Vladyslav Nekriach <ukitta555@gmail.com>
2021-04-04 11:49:41 -0200

    implement adding comments to tasks

---
+++ IntegratedCourseSystem/Controllers/CommentsController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DataBase.Models;
+using IntegratedCourseSystem;
+
+namespace IntegratedCourseSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly IntegratedCourseSystemContext _context;
+
+        public CommentsController(IntegratedCourseSystemContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Comments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Comment>>> GetComments()
+        {
+            return await _context.Comments.ToListAsync();
+        }
+
+        // GET: api/Comments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Comment>> GetComment(int id)
+        {
+            var comment = await _context.Comments.FindAsync(id);
+
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return comment;
+        }
+
+        // PUT: api/Comments/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutComment(int id, Comment comment)
+        {
+            if (id != comment.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(comment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CommentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
+        [HttpPost]
+        [Route("getByTask")]
+        public async Task<ActionResult<IEnumerable<object>>> GetByTask(DataBase.Models.Task task)
+        {
+            var comments = await _context
+                .Comments
+                .Where(comment => comment.TaskId == task.Id)
+                .Select(comment => new { 
+                    Id = comment.Id,
+                    TaskId = comment.TaskId,
+                    Text = comment.Text,
+                    Name = comment.User.Role == UserRole.Student ? comment.User.Student.Name : comment.User.Teacher.Name,
+                    Surname = comment.User.Role == UserRole.Student ? comment.User.Student.Surname : comment.User.Teacher.Surname
+                })
+                .ToListAsync();
+
+            return Created("", comments);
+        }
+
+        // POST: api/Comments
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Comment>> PostComment(Comment comment)
+        {
+            _context.Comments.Add(comment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetComment", new { id = comment.Id }, comment);
+        }
+
+
+
+        // DELETE: api/Comments/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteComment(int id)
+        {
+            var comment = await _context.Comments.FindAsync(id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Comments.Remove(comment);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CommentExists(int id)
+        {
+            return _context.Comments.Any(e => e.Id == id);
+        }
+    }
+}
